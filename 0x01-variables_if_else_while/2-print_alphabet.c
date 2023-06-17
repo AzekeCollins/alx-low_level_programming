c0a6aedf8dc1565b88f65f93f2fa48d358d90351
AzekeCollins <azekeocollins@gmail.com>
2023-06-16 21:21:32 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
 
-	for (i = 0; i < 26; i++)
-	{
-		putchar(alp[i]);
-	}
+{
+	char c;
+ 	
+	for (c = 'a' ; c <= 'z' ; c++)
+	putchar(c) ;
 	putchar('\n');
 	return (0);
 }
