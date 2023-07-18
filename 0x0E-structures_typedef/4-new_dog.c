62758fb18983359454aa64c72744e68f63cd779f
AzekeCollins <azekeocollins@gmail.com>
2023-07-18 22:01:39 +0000

    edit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -35,15 +35,8 @@ char *_strcpy(char *dest, char *src)
 	int len, i;
 
 	len = 0;
-	);
 
-		dog->name = malloc(sizeof(char) * (len1 + 1));
-			if (dog->name == NULL)
-					{
-								free(dog);
-										return (NULL);
-											}
-				while (src[len] != '\0')
+	while (src[len] != '\0')
 	{
 		len++;
 	}
@@ -75,9 +68,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-		return (NULL)
+		return (NULL);
 
-[200~}
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{
